7fd0007f75a6dfea1367df6bbe6c259270ef7311
Saumya Singh <saumya.singh@globant.com>
2024-06-13 14:45:35 +0530

    Testing commit new

--- src/test/java/javaStreams/TestJava.java
+++ src/test/java/javaStreams/TestJava.java
@@ -3,5 +3,12 @@ package javaStreams;
 public class TestJava {
     public static void main(String[] args){
         System.out.println("Hello Worldgit status");
+        System.out.println("Hello Worldgit status");
+        System.out.println("Hello Worldgit status");
+        System.out.println("Hello Worldgit status");
+        System.out.println("Hello Worldgit status");
+        System.out.println("Hello Worldgit status");
+        System.out.println("Hello Worldgit status");
+        System.out.println("Hello Worldgit status");
     }
 }
