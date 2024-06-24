cfec598e2edb34d09951a135c7b9936772e6aeef
Saumya Singh <singhsaumyait034@gmail.com>
2024-06-24 17:28:03 +0530

    Reverse Triangle

--- src/test/java/patternLogic/ReverseTrianglePattern.java
+++ src/test/java/patternLogic/ReverseTrianglePattern.java
@@ -1,4 +1,22 @@
 package patternLogic;
 
 public class ReverseTrianglePattern {
+
+    public static void main(String[] args){
+
+        for(int i=5;i>0;i--){
+            for(int j=0;j<i;j++)
+                System.out.print("x");
+            System.out.println();
+        }
+
+
+        System.out.println("***************************************");
+
+        for(int i=0;i<5;i++){
+            for(int j=5;j>i;j--)
+                System.out.print("*");
+            System.out.println();
+        }
+    }
 }
