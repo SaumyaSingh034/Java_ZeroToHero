fc7daeaa2bdb0dad0817e5786edd98e64bb8a5f0
Saumya Singh <singhsaumyait034@gmail.com>
2024-06-24 17:54:23 +0530

    Pyramid

--- src/test/java/patternLogic/RightPyramid.java
+++ src/test/java/patternLogic/RightPyramid.java
@@ -3,7 +3,7 @@ package patternLogic;
 public class RightPyramid {
     public static void main(String[] args) {
 
-        for(int i=0;i<5;i++){s
+        for(int i=0;i<5;i++){
             for(int j=4;j>i;j--){
                 System.out.print(" ");
             }
