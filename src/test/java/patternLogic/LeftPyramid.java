f5ca5c22342c6e84e168cf741649508e2267c2c5
Saumya Singh <singhsaumyait034@gmail.com>
2024-06-24 17:32:47 +0530

    Left Pyramid

--- src/test/java/patternLogic/LeftPyramid.java
+++ src/test/java/patternLogic/LeftPyramid.java
@@ -1,2 +1,19 @@
-package patternLogic;public class LeftPyramid {
+package patternLogic;
+
+public class LeftPyramid {
+
+    public static void main(String[] args){
+
+        for(int i=0;i<5;i++) {
+            for (int j = 0; j < i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        for(int l=0;l<5;l++){
+            for(int k=5;k>l;k--)
+                System.out.print("* ");
+            System.out.println();
+        }
+    }
 }
