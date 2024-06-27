372fcbe9909fdff659994dbbf8d1a3d58961f155
Saumya Singh <singhsaumyait034@gmail.com>
2024-06-27 15:49:26 +0530

    Java Interview Preparation

---
+++ src/test/java/javaInterviewPrep/PrintNumberWithoutLoop.java
@@ -0,0 +1,17 @@
+package javaInterviewPrep;
+
+public class PrintNumberWithoutLoop {
+
+    public static void main(String[] args){
+        int number = 'A'/'A';
+        int endNumber = 'd';
+        System.out.println(endNumber);
+
+        for(int i = number; i<=endNumber;i++){
+            System.out.println(i+" ");
+        }
+
+        System.out.println("---------------------------------");
+
+    }
+}
