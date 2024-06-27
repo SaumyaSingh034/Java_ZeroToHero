f690e43dc52fb2d1c848ce92d60da7713c742585
Saumya Singh <singhsaumyait034@gmail.com>
2024-06-27 16:01:21 +0530

    More Java Problems

---
+++ src/test/java/javaInterviewPrep/DivideByZero.java
@@ -0,0 +1,16 @@
+package javaInterviewPrep;
+
+public class DivideByZero {
+
+    public static void main(String[] args){
+
+        System.out.println(0.0/0.0);
+        System.out.println(9.0f/0);
+        System.out.println(9.98877727772d/0);
+        System.out.println(5/0);
+
+
+
+    }
+}
+
