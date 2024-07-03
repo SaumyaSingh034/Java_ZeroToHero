8c38157eddc4ac9ed1aeb5c6a43c9da3473f6411
Saumya Singh <singhsaumyait034@gmail.com>
2024-07-03 17:07:34 +0530

    Java streams

--- src/test/java/javaInterviewPrep/VowelCount.java
+++ src/test/java/javaInterviewPrep/VowelCount.java
@@ -1,5 +1,7 @@
 package javaInterviewPrep;
 
+import java.util.function.IntPredicate;
+
 public class VowelCount {
 
     public static void main(String[] args) {
@@ -11,6 +13,19 @@ public class VowelCount {
                 count++;
         }
         System.out.println("Typical Java Method "+ count);
+
+        System.out.println("Using Java Streams");
+        IntPredicate vowel = new IntPredicate() {
+            @Override
+            public boolean test(int t) {
+                return t == 'a' || t == 'e' || t == 'i' || t == 'o' || t == 'u'
+                ||t == 'A' || t == 'E' || t == 'I' || t == 'O' || t == 'U';
+            }
+        };
+        String test = "aeiouAEIOU";
+        long vCount = test.chars().filter(vowel).count();
+        System.out.println(vCount);
+
 
 
     }
