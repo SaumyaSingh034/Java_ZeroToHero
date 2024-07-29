0b9f8e3194aea0b7106a19aa890ffd856104c56e
Saumya Singh <singhsaumyait034@gmail.com>
2024-07-29 10:55:16 +0530

    Calling main method from another class

---
+++ src/test/java/javaSessionsNaveen/Example1.java
@@ -0,0 +1,8 @@
+package javaSessionsNaveen;
+
+public class Example1 {
+    public static void main(String[] args){
+        System.out.println("Inside Example1");
+        //Example2.main(args);
+    }
+}
---
+++ src/test/java/javaSessionsNaveen/Example2.java
@@ -0,0 +1,8 @@
+package javaSessionsNaveen;
+
+public class Example2 {
+    public static void main(String[] args){
+        System.out.println("Inside Example2");
+        Example1.main(args);
+    }
+}
