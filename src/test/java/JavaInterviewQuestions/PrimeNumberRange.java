f313d5be2840116ec74153abb51ea03a6bb6cc81
Saumya Singh <singhsaumyait034@gmail.com>
2024-07-31 17:38:35 +0530

    Prime Number In Range

--- src/test/java/JavaInterviewQuestions/PrimeNumberRange.java
+++ src/test/java/JavaInterviewQuestions/PrimeNumberRange.java
@@ -5,5 +5,21 @@ public class PrimeNumberRange {
     public static void main(String[] args){
         int start = 1;
         int end = 20;
+        int flag;
+
+        for(int i = start; i<=end;i++){
+            if(i==1||i==0)
+                continue;
+            flag = 1;
+            for(int j=2;j<=i/2;j++){
+                if(i % j == 0){
+                    flag=0;
+                    break;
+                }
+            }
+            if(flag ==1)
+                System.out.print(i+" ");
+
+        }
     }
 }
