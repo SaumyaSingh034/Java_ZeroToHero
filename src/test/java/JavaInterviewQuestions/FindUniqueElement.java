78fc50f9181a33de3a1f9715ec055adb986cbeb5
Saumya Singh <singhsaumyait034@gmail.com>
2024-07-31 17:23:32 +0530

    Unique Element

---
+++ src/test/java/JavaInterviewQuestions/FindUniqueElement.java
@@ -0,0 +1,26 @@
+package JavaInterviewQuestions;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+public class FindUniqueElement {
+
+    public static void main(String[] args){
+        int[] arr = {4,5,5,7,6,6,7,4,9,5};
+        Map<Integer, Integer> dataMap = new HashMap<>();
+
+        for(int i=0;i< arr.length;i++){
+            if(dataMap.containsKey(arr[i]))
+                dataMap.put(arr[i], dataMap.get(arr[i])+1);
+            else
+                dataMap.put(arr[i], 1);
+        }
+        System.out.println(dataMap);
+        Set<Map.Entry<Integer, Integer>> keyData = dataMap.entrySet();
+        for(Map.Entry<Integer, Integer> en : keyData) {
+            if (en.getValue() == 1)
+                System.out.println(en.getKey()+" ");
+        }
+    }
+}
---
+++ src/test/java/JavaInterviewQuestions/PrimeNumberRange.java
@@ -0,0 +1,9 @@
+package JavaInterviewQuestions;
+
+public class PrimeNumberRange {
+
+    public static void main(String[] args){
+        int start = 1;
+        int end = 20;
+    }
+}
