c7ba2cb12503738514cb73da8abd6be1079bc61a
Saumya Singh <singhsaumyait034@gmail.com>
2024-08-01 11:14:58 +0530

    Print twice Letter

--- src/test/java/JavaInterviewQuestions/TwicePrintLetter.java
+++ src/test/java/JavaInterviewQuestions/TwicePrintLetter.java
@@ -3,7 +3,13 @@ package JavaInterviewQuestions;
 public class TwicePrintLetter {
 
     public static void main(String[] args) {
-        String data = "hello";
+        String data = "hello   ";
+        String dataNew = "";
         //output = hheelllloo
+        for(int i = 0;i<data.length();i++){
+            for(int j=1;j<=2;j++)
+                dataNew = dataNew+data.charAt(i);
+        }
+        System.out.println(dataNew);
     }
 }
