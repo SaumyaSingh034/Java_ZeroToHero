2411b1d7d8ef6004db71bb290a7d7008a7f552a6
Saumya Singh <singhsaumyait034@gmail.com>
2024-08-01 11:45:30 +0530

    Removing Duplicate Characters

--- src/test/java/JavaInterviewQuestions/RemoveDuplicateChars.java
+++ src/test/java/JavaInterviewQuestions/RemoveDuplicateChars.java
@@ -1,9 +1,25 @@
 package JavaInterviewQuestions;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class RemoveDuplicateChars {
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         String data = "characters";
-        //output = chartes
+        char[] ch = data.toCharArray();
+        System.out.println(removeDuplicates(ch));
     }
+
+    private static String removeDuplicates(char[] ch) {
+        Map<Character, Integer> hashMap = new HashMap<>();
+        String newData = "";
+        for(int i=0;i< ch.length;i++){
+            if(!hashMap.containsKey(ch[i])){
+                newData += ch[i];
+                hashMap.put(ch[i], 1);
+            }
+        }
+        return newData;
+        }
 }
