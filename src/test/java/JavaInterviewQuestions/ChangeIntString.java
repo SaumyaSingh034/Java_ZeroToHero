a1ee26ec70a9216d55178bcdab681934802ca3dd
Saumya Singh <singhsaumyait034@gmail.com>
2024-08-03 11:39:27 +0530

    Integer to String

---
+++ src/test/java/JavaInterviewQuestions/ChangeIntString.java
@@ -0,0 +1,12 @@
+package JavaInterviewQuestions;
+
+public class ChangeIntString {
+
+    public static void main(String[] args){
+        int x = 90;
+        System.out.println("Integer Value : "+x);
+        String str = String.valueOf(x);
+        System.out.println("String Value of : "+str);
+
+    }
+}
