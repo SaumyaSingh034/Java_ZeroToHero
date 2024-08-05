25bf14a4ad8fa076193f8063299bb281eaccacbc
Saumya Singh <singhsaumyait034@gmail.com>
2024-08-05 15:51:33 +0530

    Valid main Method

---
+++ src/test/java/basicJavaInterview/DurgaMainMethod.java
@@ -0,0 +1,8 @@
+package basicJavaInterview;
+
+public class DurgaMainMethod {
+
+    final static synchronized strictfp public void main(String... saumya){
+        System.out.println("Hello Saumya from Valid Main method");
+    }
+}
