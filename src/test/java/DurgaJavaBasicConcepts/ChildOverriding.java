5f40fbca86c1594d82b100917dc83c1efb12d630
Saumya Singh <singhsaumyait034@gmail.com>
2024-08-05 16:11:48 +0530

    Main method Overriding concept

---
+++ src/test/java/DurgaJavaBasicConcepts/ChildOverriding.java
@@ -0,0 +1,8 @@
+package DurgaJavaBasicConcepts;
+
+public class ChildOverriding extends ParentOverriding{
+
+    public static void main(String[] args){
+        System.out.println("Child Class Overriding Concept");
+    }
+}
---
+++ src/test/java/DurgaJavaBasicConcepts/ParentOverriding.java
@@ -0,0 +1,7 @@
+package DurgaJavaBasicConcepts;
+
+public class ParentOverriding {
+    public static void main(String[] args){
+        System.out.println("Hello Parent Overriding Concept");
+    }
+}
