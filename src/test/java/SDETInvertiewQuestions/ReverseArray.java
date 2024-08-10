974ef1ecef05275a769f4e1a7d0cc56cf39c4c23
Saumya Singh <singhsaumyait034@gmail.com>
2024-08-10 16:10:23 +0530

    Reverse Array

---
+++ src/test/java/SDETInvertiewQuestions/ReverseArray.java
@@ -0,0 +1,25 @@
+package SDETInvertiewQuestions;
+
+public class ReverseArray {
+    public static void main(String[] args){
+        int[] arr = {1,2,3,4,5,6,7,8,9};
+        reverseArrayUsingForLoop(arr);
+    }
+
+    private static void reverseArrayUsingForLoop(int[] arr) {
+        int right = arr.length-1;
+        int left = 0;
+        while(left<right){
+            int i = arr[left];
+            arr[left] = arr[right];
+            arr[right] = i;
+
+            left +=1;
+            right -=1;
+        }
+        for(int i=0;i< arr.length;i++){
+            System.out.print(arr[i]+" ");
+        }
+    }
+
+}
