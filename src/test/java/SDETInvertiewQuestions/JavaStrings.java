c75b3c1c8cbf9025f0b5ded534e89f9ecf37ea5e
Saumya Singh <singhsaumyait034@gmail.com>
2024-08-16 16:36:07 +0530

    Customized String

---
+++ src/test/java/SDETInvertiewQuestions/JavaStrings.java
@@ -0,0 +1,12 @@
+package SDETInvertiewQuestions;
+
+public class JavaStrings {
+
+    public static void main(String[] args){
+        String a = "Latest Version";
+        String b = "of Selenium";
+        String c = "3";
+        System.out.println("We are learning Java Selenium"+" and the "+a+" "+b+" "+c);
+
+    }
+}
---
+++ src/test/java/SDETInvertiewQuestions/LinkedIn1.java
@@ -0,0 +1,65 @@
+package SDETInvertiewQuestions;
+
+public class LinkedIn1 {
+    public static void main(String[] args){
+        String s1 = "Go to Joho";
+        //output = G$ t$$ J$$$h$$$$$
+        System.out.println(printStringWithGivenFormat(s1,"$","o"));
+        System.out.println(test(s1));
+    }
+
+    public static String customizeString(String s, String specialchar, String chartobereplace) {
+        String arr=s;
+        int len=0;
+        StringBuilder sb=new StringBuilder();
+        for(int i=0;i<arr.length();i++) {
+            if(Character.toString(arr.charAt(i)).equals(chartobereplace)) {
+                len++;
+                for(int j=0;j<len;j++) {
+                    sb.append(specialchar);
+                }
+            }
+            else {
+                sb.append(arr.charAt(i));
+            }
+        }
+        return sb.toString();
+
+    }
+
+
+    private static String printStringWithGivenFormat(String s, String specialChar, String charToreplace) {
+        String arr = s;
+        int length = 0;
+        StringBuilder sb = new StringBuilder();
+        for(int i=0;i<arr.length();i++){
+            if(Character.toString(arr.charAt(i)).equalsIgnoreCase(charToreplace)){
+                length++;
+                for(int j =0;j<length;j++){
+                    sb.append(specialChar);
+                }
+
+            }else {
+                sb.append(arr.charAt(i));
+            }
+        }
+        return sb.toString();
+
+        }
+        public static String test(String s1){
+        StringBuffer sb = new StringBuffer();
+        int count = 0;
+        for(int i=0;i<s1.length();i++){
+            if(s1.charAt(i)=='o'){
+                count++;
+                for(int j=1;j<=count;j++){
+                    sb.append("$");
+                }
+            } else{
+                sb.append(s1.charAt(i));
+            }
+        }
+        return sb.toString();
+        }
+
+}
