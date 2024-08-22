36aeb03c6c03cc8e262817ce572ec2a83bdd4b32
Saumya Singh <singhsaumyait034@gmail.com>
2024-08-22 11:38:45 +0530

    Leap Year

---
+++ src/test/java/hackerRank/LeapYear.java
@@ -0,0 +1,19 @@
+package hackerRank;
+
+public class LeapYear {
+    public static void main(String[] args) {
+        int year = 2021;
+        if (year % 4 == 0) {
+            if (year % 100 == 0) {
+                if (year % 400 == 0) {
+                    System.out.println("Leap year");
+                } else
+                    System.out.println("Not a leap Year");
+            } else
+                System.out.println("Leap Year");
+        } else {
+            System.out.println("Not a leap Year");
+        }
+
+    }
+}
