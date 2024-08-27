e129affefc298ed5aa2d0f2869378ae59f5a15a3
Saumya Singh <singhsaumyait034@gmail.com>
2024-08-27 14:24:05 +0530

    Star Pyramid

--- src/test/java/SDET_RahulShetty/StarPyramid2.java
+++ src/test/java/SDET_RahulShetty/StarPyramid2.java
@@ -1,11 +1,10 @@
 package SDET_RahulShetty;
 
-public class StartPyramid1 {
-
+public class StarPyramid2 {
     public static void main(String[] args){
         for(int i=1;i<=5;i++){
-            for(int j = 1;j<=i;j++){
-                System.out.print("  * ");
+            for(int j=5;j>=i;j--){
+                System.out.print(" * ");
             }
             System.out.println();
         }
