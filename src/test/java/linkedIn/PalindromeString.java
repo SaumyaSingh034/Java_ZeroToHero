50ab7149a21129fb6f36715acbed33026b90ebf2
Saumya Singh <singhsaumyait034@gmail.com>
2024-08-28 14:51:10 +0530

    Palindrome String

---
+++ src/test/java/linkedIn/PalindromeString.java
@@ -0,0 +1,25 @@
+package linkedIn;
+
+public class PalindromeString {
+
+    public static void main(String[] args){
+        PalindromeString ps = new PalindromeString();
+        ps.StringPalindrome("121");
+        ps.StringPalindrome("madam");
+        ps.StringPalindrome("");
+        ps.StringPalindrome("A");
+        ps.StringPalindrome("Ana");
+        ps.StringPalindrome("Saumya");
+    }
+
+    protected void StringPalindrome(String str){
+        StringBuilder sb = new StringBuilder(str);
+        String rev = sb.reverse().toString();
+        if(rev.equalsIgnoreCase(str)){
+            System.out.println("Palindrome String : String --> "+str+" Revsre String : "+rev);
+        }
+        else{
+            System.out.println("Not Palindrome String : String --> "+str+" Revsre String : "+rev);
+        }
+    }
+}
