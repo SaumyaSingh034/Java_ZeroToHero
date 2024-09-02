1c86dba11ceb99ec941bdd51ae3c0672c1223153
Saumya Singh <singhsaumyait034@gmail.com>
2024-09-02 12:20:02 +0530

    Abstract Class

---
+++ src/test/java/AbstractInterface/AbstractCarname.java
@@ -0,0 +1,7 @@
+package AbstractInterface;
+
+public abstract class AbstractCarname implements Car{
+    public void carName(){
+        System.out.println("Hello Car Name from Abstract class");
+    }
+}
