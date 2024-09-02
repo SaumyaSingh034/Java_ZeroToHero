c6fc45cb16314ecba7d0be4b212cefe9ce1e21c0
Saumya Singh <singhsaumyait034@gmail.com>
2024-09-02 15:44:31 +0530

    Remove Duplicate

---
+++ src/main/java/RemoveDuplicateFromList.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class RemoveDuplicateFromList {
+
+    public static void main(String[] args){
+        List<Object> list = List.of( 0, 1, 2, 3, 5, 6, 0, 0, 1 );
+        System.out.println(list+" ---> "+list.size());
+        list = Arrays.asList(list.stream().distinct().toArray());
+        System.out.println(list+" ---> "+list.size());
+
+
+    }
+}
---
+++ src/main/java/WaysOfCreatingString.java
@@ -0,0 +1,8 @@
+public class WaysOfCreatingString {
+
+    public static void main(String[] args){
+        char[] ch = {'j','a','v','a'};
+        String s = new String(ch);
+        System.out.println(s);
+    }
+}
