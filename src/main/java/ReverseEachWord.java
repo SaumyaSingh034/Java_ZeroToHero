1ba028a177d71d8f99ccbb34f5439eb3cae75b6d
Saumya Singh <singhsaumyait034@gmail.com>
2024-09-02 17:10:25 +0530

    Reverse Each Word

---
+++ src/main/java/ReverseEachWord.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class ReverseEachWord {
+
+    public static void main(String[] args){
+        String s1 = "I am a Software Tester";
+
+        reverseEachWordInString(s1);
+    }
+
+    private static void reverseEachWordInString(String s1) {
+        String[] array = s1.split(" ");
+        StringBuffer sb = new StringBuffer();
+        for(String word : array){
+            StringBuffer reverse = new StringBuffer(word);
+            reverse.reverse();
+            sb.append(reverse).append(" ");
+        }
+
+        System.out.println(sb);
+
+    }
+}
