21b5d6677f8154c1dd1e030acf018215b63be0ca
Saumya Singh <singhsaumyait034@gmail.com>
2024-09-16 12:35:33 +0530

    Factorial

---
+++ src/main/java/JSON_Jackson/Fcat.java
@@ -0,0 +1,13 @@
+package JSON_Jackson;
+
+public class Fcat {
+
+    public static void main(String[] args) {
+        int number = 5;
+        int multi = 1;
+        for (int i = 1; i <= number; i++) {
+            multi = multi * i;
+        }
+        System.out.println(multi);
+    }
+}
