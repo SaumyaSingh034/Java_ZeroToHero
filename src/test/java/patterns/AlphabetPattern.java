50f7ea2049094f0c48ad62ec5a90ab2119248e04
Saumya Singh <singhsaumyait034@gmail.com>
2024-09-17 21:41:59 +0530

    Alphabet Pyramid With A AB ABC ABCD Pattern

---
+++ src/test/java/patterns/AlphabetPattern.java
@@ -0,0 +1,14 @@
+package patterns;
+
+public class AlphabetPattern {
+
+    public static void main(String[] args){
+        int a = 65;
+        for(int i=0;i<=5;i++){
+            for(int j=0;j<=i;j++){
+                System.out.print((char) (a+i)+" ");
+            }
+            System.out.println();
+        }
+    }
+}
