0badc936f93994c1152079397235dfc2c484b39e
Saumya Singh <singhsaumyait034@gmail.com>
2024-09-27 22:53:02 +0530

    Min Value in HashMap

---
+++ src/test/java/VIMO_Fitment/MinVal.java
@@ -0,0 +1,34 @@
+package VIMO_Fitment;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+public class MinVal {
+
+    public static void main(String[] args){
+        Map<String, Integer> data = new HashMap<>();
+        Integer min;
+        String name;
+        data.put("Rohit",30);
+        data.put("Virat",27);
+        data.put("Jadega",25);
+        data.put("Ashwin",34);
+        data.put("Shuman",31);
+
+         min = Collections.min(data.values());
+        System.out.println(min);
+
+
+        Map.Entry<String, Integer> min1 = null;
+        for(Map.Entry<String, Integer> m : data.entrySet()){
+            if(min1 == null || m.getValue() < min1.getValue()){
+                min1= m;
+            }
+
+        }
+        System.out.println(min1.getKey()+" -----> "+min1.getValue());
+
+
+    }
+}
