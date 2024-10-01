6ce916e5fee36875bbd2a047fad4edc95edd1dbd
Saumya Singh <singhsaumyait034@gmail.com>
2024-10-01 21:36:51 +0530

    Pattern Questions

---
+++ src/main/java/PatternProgram.java
@@ -0,0 +1,8 @@
+public class PatternProgram {
+    public static void main(String[] args){
+        String str = "SAUMYA";
+       for(int i=str.length();i>0;i--){
+           System.out.println(str.substring(0,i));
+       }
+    }
+}
