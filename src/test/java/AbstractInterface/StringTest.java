7b55506789f31a37c86b9b7e4594420afeaa1c09
Saumya Singh <singhsaumyait034@gmail.com>
2024-10-15 23:30:21 +0530

    test

--- src/test/java/AbstractInterface/StringTest.java
+++ src/test/java/AbstractInterface/StringTest.java
@@ -1,33 +1,37 @@
 package AbstractInterface;
 
 public class StringTest {
-        public static void main(String[] args) {
-            String s = "123";
-            String s1 = "8643556";
 
-            // Initialize a StringBuilder to store the result
-            StringBuilder result = new StringBuilder();
-
-            // Iterate through the characters of s1
-            for (int i = 0; i < s1.length(); i++) {
-                char charS1 = s1.charAt(i);
-                char charS;
-
-                // If the index is within the bounds of s, subtract the corresponding characters
-                if (i < s.length()) {
-                    charS = s.charAt(i);
-                    // Calculate the new character by subtracting and adding '0' to convert back to char
-                    char newChar = (char) ('0' + (charS1 - '0') + (10 - (charS - '0')) % 10);
-                    result.append(newChar);
-                } else {
-                    // If s has been fully processed, append the remaining characters from s1
-                    result.append(charS1);
-                }
+    private static void test(String s1, String s2) {
+        char[] c1 = s1.toCharArray();
+        char[] c2 = s2.toCharArray();
+        StringBuffer sb = new StringBuffer();
+        int sum;
+        int a;
+        int b;
+        for (int i = 0; i < c2.length; i++) {
+            for (int j = 0; j < c1.length; j++) {
+                a = Integer.parseInt(String.valueOf(c1[j]));
+                b = Integer.parseInt(String.valueOf(c2[i]));
+                i++;
+                sum = a + b;
+                sb.append(sum);
             }
+            break;
 
-            // Print the final result
-            System.out.println(result.toString());
         }
+        for (int i = c1.length; i < c2.length; i++) {
+            sb.append(c2[i]);
+        }
+        System.out.println(sb);
     }
+
+    public static void main(String[] args) {
+        String s = "123";
+        String s1 = "8643556";
+        test(s, s1);
+
+    }
+}
 
 
