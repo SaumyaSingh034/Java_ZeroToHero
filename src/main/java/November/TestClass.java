3ff80973bd8d040b90023096401d48b464c42768
Saumya Singh <singhsaumyait034@gmail.com>
2024-11-12 12:42:49 +0530

    Singleton Design Pattern

---
+++ src/main/java/November/TestClass.java
@@ -0,0 +1,9 @@
+package November;
+
+public class TestClass {
+
+    public static void main(String[] args){
+        WebDriverManager webDriverManager;
+        webDriverManager = WebDriverManager.getDriverManager();
+    }
+}
