0019e7eb9a0059c02cf6ef24cc0bbe4ae0963d43
Saumya Singh <singhsaumyait034@gmail.com>
2024-11-13 18:35:31 +0530

    Vowel Count Word

--- src/main/java/BrightlySoftware/VowelCountWord.java
+++ src/main/java/BrightlySoftware/VowelCountWord.java
@@ -4,6 +4,7 @@ public class VowelCountWord {
     public static void main(String[] args){
         String str = "Java Programming is best language tytyty";
         countVowelWords(str);
+        countVowelWordsThroughChatGPT("Saumya Singh ghwty");
     }
 
     private static void countVowelWords(String str) {
@@ -17,5 +18,24 @@ public class VowelCountWord {
             }
         }
         System.out.println(count);
+    }
+
+    private static void countVowelWordsThroughChatGPT(String str){
+        String[] words = str.split(" ");
+        int count=0;
+        for(String word : words){
+            if(containsVowel(word)){
+                count++;
+            }
+        }
+        System.out.println(count);
+    }
+
+    private static boolean containsVowel(String word) {
+        for(char c : word.toLowerCase().toCharArray()){
+            if("aeiou".indexOf(c)!=-1)
+                return true;
+        }
+        return false;
     }
 }
